3fea0b5b3c5c9d67c903f57f97fabc838e1589f3
At-lien313 <esteban.legrand@outlook.fr>
2019-10-05 01:01:00 +0200

    Déplacements personnage Top Down

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] private float moveSpeed = 5.0f;
+    private Vector2 movement;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        GetInput();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void GetInput()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal1");
+        movement.y = Input.GetAxisRaw("Vertical1");
+    }
+
+    void Move()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
