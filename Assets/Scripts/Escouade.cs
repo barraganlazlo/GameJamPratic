dfd200e3e66d6c342e398e29685e44768992cd84
lazlo barragan <lazlo.barragan@gmail.com>
2019-10-05 12:11:57 +0200

    unit editor system

---
+++ Assets/Scripts/Escouade.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Escouade : MonoBehaviour
+{
+    EscouadeType type;
+
+    Unit[,] units;
+
+    public void SetType(EscouadeType type)
+    {
+        this.type = type;
+    }
+    public void Instantiate()
+    {
+        units = new Unit[type.units.GetLength(0), type.units.GetLength(1)];
+        for (int i = 0; i < units.GetLength(0); i++)
+        {
+            for (int j = 0; j < units.GetLength(1); j++)
+            {
+                GameObject unitGO = Instantiate<GameObject>(UnitManager.instance.unitTypes[type.units[i, j]].prefab);
+                units[i, j] = unitGO.GetComponent<Unit>();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UnitManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UnitManager : MonoBehaviour
+{
+    public static UnitManager instance;
+
+    [HideInInspector]
+    public EscouadeType[] escouadeTypes;
+    [HideInInspector]
+    public UnitType[] unitTypes;
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("There are Multiples UnitManager in the scene but it can only be one ");
+        }
+        instance = this;
+    }
+
+    private void LoadUnitTypes()
+    {
+        unitTypes = Resources.LoadAll<UnitType>("UnitTypes");
+    }
+    private void LoadEscouadeTypes()
+    {
+        unitTypes = Resources.LoadAll<UnitType>("UnitTypes");
+    }
+
+}
