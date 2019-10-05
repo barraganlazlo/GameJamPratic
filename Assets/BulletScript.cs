d0a4870e50565c6c0a62f551b735bfa866b99991
AmauryHyde <amauryhyde@gmail.com>
2019-10-05 13:14:41 +0200

    SFX et projectils P2
    UI element + projectils

---
+++ Assets/BulletScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    public float speed = 3.0f;
+    public float LifeTime = 4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, LifeTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.left * Time.deltaTime * speed);
+    }
+}
