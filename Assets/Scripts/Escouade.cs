706bacd684a3664b4b8ce225712ffecf922dc310
lazlo barragan <lazlo.barragan@gmail.com>
2019-10-05 22:47:36 +0200

    attackspeed modify and empty unit in escouade

--- Assets/Scripts/Escouade.cs
+++ Assets/Scripts/Escouade.cs
@@ -22,18 +22,22 @@ public class Escouade : MonoBehaviour
             for (int j = 0; j < type.width; j++)
             {
                 int uId = (type.height * type.width - 1) - (i * type.width + j);
-                UnitType unitType = UnitManager.instance.unitTypes[type.units[uId]];
-                GameObject unitGO = Instantiate<GameObject>(unitType.prefab, transform.position, transform.rotation, transform);
-                Vector3 pos = transform.position;
-                float hSpace = verticalMargin + unitGO.transform.localScale.x;
-                float vSpace = horizontalMargin + unitGO.transform.localScale.y;
-                pos.y -= (type.height - 1) * hSpace * 0.5f;
-                pos.x -= (type.width - 1) * vSpace * 0.5f;
-                pos.x += j * hSpace;
-                pos.y += i * vSpace;
-                unitGO.transform.position = pos;
-                units[i, j] = unitGO.GetComponent<Unit>();
-                units[i, j].escouade = this;
+                if (type.units[uId]>-1)
+                {
+                    UnitType unitType = UnitManager.instance.unitTypes[type.units[uId]];
+                    GameObject unitGO = Instantiate<GameObject>(unitType.prefab, transform.position, transform.rotation, transform);
+                    Vector3 pos = transform.position;
+                    float hSpace = verticalMargin + unitGO.transform.localScale.x;
+                    float vSpace = horizontalMargin + unitGO.transform.localScale.y;
+                    pos.y -= (type.height - 1) * hSpace * 0.5f;
+                    pos.x -= (type.width - 1) * vSpace * 0.5f;
+                    pos.x += j * hSpace;
+                    pos.y += i * vSpace;
+                    unitGO.transform.position = pos;
+                    units[i, j] = unitGO.GetComponent<Unit>();
+                    units[i, j].escouade = this;
+                }
+               
             }
         }
         transform.rotation = spawner.transform.rotation;
