6043fab37b839134003b2d7a62fa00fd65c870e5
Luxgnite <ewan.dubois@live.fr>
2019-10-06 10:09:26 +0200

    Passage AudioManager en Singleton + quelques ajustements

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -6,9 +6,20 @@ public class AudioManager : MonoBehaviour
 {
     public Sound[] sounds;
 
-    // Start is called before the first frame update
-    void Start()
+    public static AudioManager instance;
+
+    void Awake()
     {
+
+        if (instance == null)
+            instance = this;
+        else
+        { 
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
 
         foreach (Sound s in sounds)
         {
@@ -17,6 +28,7 @@ public class AudioManager : MonoBehaviour
 
             s.source.volume = s.volume;
             s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
         }
         
     }
@@ -24,6 +36,10 @@ public class AudioManager : MonoBehaviour
     public void Play (string name)
     {
         Sound s = Array.Find(sounds, sound => sound.name == name);
+        if(s == null)
+        {
+            Debug.LogWarning("Le son " + name + " n'a pas été trouvé.");
+        }
         s.source.Play();
     }
 
